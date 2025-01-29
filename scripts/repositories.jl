6781b801455cf21a597fb6c301af5cc1a62d3b7f
Ivet Galabova <galabovaa@gmail.com>
2025-01-29 20:01:32 +0000

    generating data.json. next see how to visualize

--- scripts/repositories.jl
+++ scripts/repositories.jl
@@ -21,7 +21,7 @@ if isfile(joinpath(@__DIR__, "dev.env"))
     println("Loaded GH secret IG")
 end
 
-const DATA_DIR = joinpath(dirname(@__DIR__), "docs", "repositories")
+const DATA_DIR = joinpath(dirname(@__DIR__), "repositories")
 
 function Repository(repo; since, until, my_auth)
     println("Getting : ", repo)
@@ -45,50 +45,6 @@ function get_repos(since, until)
     )
 end
 
-function download_stats(file)
-    url = "https://julialang-logs.s3.amazonaws.com/public_outputs/current/$(file).csv.gz"
-    output = joinpath(dirname(@__DIR__), "data", "$(file).csv.gz")
-    Downloads.download(url, output)
-    return output
-end
-
-function load_stats(file, uuids)
-    out = download_stats(file)
-    df = CSV.read(out, DataFrames.DataFrame)
-    uuid_to_name = DataFrames.DataFrame(
-        package_uuid = collect(keys(uuids)),
-        name = collect(values(uuids)),
-    )
-    df = DataFrames.leftjoin(df, uuid_to_name; on = :package_uuid)
-    filter!(df) do row
-        return !ismissing(row.client_type) &&
-               row.client_type == "user" &&
-               !ismissing(row.name) &&
-               occursin("ERGO-Code/", row.name) &&
-               row.status in (200, 301, 302)
-    end
-    return DataFrames.select(df, [:name, :date, :request_count])
-end
-
-function get_historical_downloads(
-    filename::String = joinpath(DATA_DIR, "download_stats.json"),
-)
-    current = JSON.parsefile(filename; use_mmap = false)
-    name = String[]
-    date = Dates.Date[]
-    request_count_sum = Int[]
-    for (pkg, results) in current
-        append!(name, fill("ERGO-Code/$pkg", length(results["requests"])))
-        append!(date, Dates.Date.(results["dates"]))
-        append!(request_count_sum, results["requests"])
-    end
-    return DataFrames.DataFrame(
-        name = name,
-        date = date,
-        request_count_sum = request_count_sum,
-    )
-end
-
 function get_pkg_uuids()
     pkg_uuids = Dict{String,String}()
     r = first(Pkg.Registry.reachable_registries())
@@ -101,28 +57,6 @@ function get_pkg_uuids()
     return pkg_uuids
 end
 
-function update_download_statistics()
-    pkg_uuids = get_pkg_uuids()
-    df = load_stats("package_requests_by_region_by_date", pkg_uuids)
-    new_df = sort!(combine(groupby(df, [:name, :date]), :request_count => sum))
-    new_df.name = String.(new_df.name)
-    current = get_historical_downloads()
-    append!(current, new_df)
-    unique!(current)
-    sort!(current, [:name, :date])
-    data = Dict{String,Dict{String,Any}}()
-    for g in groupby(current, :name)
-        key = replace(g[1, :name], "ERGO-Code/" => "")
-        data[key] = Dict{String,Any}(
-            "dates" => string.(collect(g.date)),
-            "requests" => collect(g.request_count_sum),
-        )
-    end
-    open(joinpath(DATA_DIR, "download_stats.json"), "w") do io
-        return write(io, JSON.json(data))
-    end
-    return
-end
 
 function update_package_statistics()
     since = "2013-01-01T00:00:00"
@@ -246,50 +180,12 @@ function prs_by_user(user)
     return prs_by_user
 end
 
-# function state_of_jump_statistics()
-#     old_date = Dates.today() - Dates.Year(1)
-#     # Downloads
-#     df = get_historical_downloads()
-#     n_downloads = sum(df[df.date.>=old_date, :].request_count_sum)
-#     # PRs and issues
-#     data = JSON.parsefile(joinpath(DATA_DIR, "data.json"))
-#     prs_opened, issues_opened, contributors = 0, 0, Set{String}()
-#     for (pkg, items) in data, item in items
-#         if Dates.DateTime(item["date"]) >= old_date && item["type"] == "opened"
-#             if item["is_pr"]
-#                 push!(contributors, item["user"])
-#                 prs_opened += 1
-#             else
-#                 issues_opened += 1
-#             end
-#         end
-#     end
-#     open(joinpath(DATA_DIR, "summary.json"), "w") do io
-#         summary = Dict(
-#             "n_downloads" => n_downloads,
-#             "prs_opened" => prs_opened,
-#             "issues_opened" => issues_opened,
-#             "num_contributors" => length(contributors),
-#         )
-#         write(io, JSON.json(summary))
-#         return
-#     end
-#     println("""
-#     Downloads            : >$n_downloads
-#     Pull requests opened : $prs_opened
-#     Issues opened        : $issues_opened
-#     Unique contributors  : $(length(contributors))
-#     """)
-#     return
-# end
 
 has_arg(arg) = any(isequal(arg), ARGS)
 
 if has_arg("--update")
-    # update_download_statistics()
     update_package_statistics()
-    # update_contributor_prs_over_time()
-    # # state_of_jump_statistics()
+    update_contributor_prs_over_time()
 end
 
 
